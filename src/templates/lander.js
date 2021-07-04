ee642efe0a5abeda43193769d100b09718ad1d3f
itsvitoracacio <vitor.acacio@pm.me>
2021-07-04 17:39:41 -0300

    Updated subheadline on the services page

--- src/templates/lander.js
+++ src/templates/lander.js
@@ -17,10 +17,12 @@ const Lander = ({ children }) => {
                   <path d="M6.9739 30.8153H63.0244C65.5269 30.8152 75.5358 -3.68471 35.4998 2.81531C-16.1598 11.2025 0.894099 33.9766 26.9922 34.3153C104.062 35.3153 54.5169 -6.68469 23.489 9.31527" />
                 </svg>
               </span>{" "}
-              with online{" "}
-              <span class="yellow-u">marketing & sales?</span>
+              with online <span class="yellow-u">marketing & sales?</span>
             </h1>
-            <h2>Get your high-converting, lightning-fast website built by a marketing pro.</h2>
+            <h2>
+              You might be wasting your time and money trying to make it work on a broken site. Get your <strong>high-converting, lightning-fast website</strong> built by a
+              marketing pro.
+            </h2>
             <div>
               <button type="button">
                 <Link to="/">Get a free audit of your website</Link>
